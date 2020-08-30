519228336eac56ea78ca1251341bde8f70a669ec
Andre Furlan <ensismoebius@gmail.com>
2020-08-30 01:53:43 -0300

    No need for git modules anymore for now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use easy_rss::RssParser;
 
-fn main()->Result<(),Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let address = "https://www.zhihu.com/rss";
-    let mut parser = RssParser::from_url(address,"utf8")?;
+    let mut parser = RssParser::from_url(address, "utf8")?;
     parser.author_tag = String::from("dc:creator");
     assert!(parser.parse_json().is_ok());
     Ok(())
