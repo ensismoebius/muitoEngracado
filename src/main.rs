703dbc7ed8330578a8c9a5539e2b3735a132f9b4
Andre Furlan <ensismoebius@gmail.com>
2020-08-30 00:42:15 -0300

    Will use Rust instead of C++
    Lets give it a chance ;)

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use easy_rss::RssParser;
+
+fn main()->Result<(),Box<dyn std::error::Error>> {
+    let address = "https://www.zhihu.com/rss";
+    let mut parser = RssParser::from_url(address,"utf8")?;
+    parser.author_tag = String::from("dc:creator");
+    assert!(parser.parse_json().is_ok());
+    Ok(())
+}
