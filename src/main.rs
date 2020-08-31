0aee63346ab3b0a404bbbecb63264f8f0aa9295f
Andre Furlan <ensismoebius@gmail.com>
2020-08-31 13:50:52 -0300

    Testing vscodium

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let address = "https://www.zhihu.com/rss";
     let mut parser = RssParser::from_url(address, "utf8")?;
     parser.author_tag = String::from("dc:creator");
-    assert!(parser.parse_json().is_ok());
-    Ok(())
 }
